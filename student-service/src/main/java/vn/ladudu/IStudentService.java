98d540e42582ca9a562dcc4721a5faaf24dff301
Thuy <dtranthuy1995@gmail.com>
2019-09-25 09:35:43 +0700

    spring boot restapi structure

---
+++ student-service/src/main/java/vn/ladudu/IStudentService.java
@@ -0,0 +1,13 @@
+package vn.ladudu;
+
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IStudentService {
+    List<Student> getAll();
+    Optional<Student> getById(Long id);
+    boolean create(Student student);
+    boolean edit(Student student);
+    boolean delete(Long id);
+}
